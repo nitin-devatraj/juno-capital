f9a43bb7618808ffa9514dda7b4f8d7c5d4e18a5
nitin devatraj <nitindevatraj@protonmail.com>
2024-01-25 05:23:03 +0530

    implement AccountOperations, BalanceDisplay components

---
+++ src/features/accounts/BalanceDisplay.js
@@ -0,0 +1,12 @@
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+function BalanceDisplay() {
+  return <div className="balance">{formatCurrency(1000)}</div>;
+}
+
+export default BalanceDisplay;
