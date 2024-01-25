056c35ba27d318e71813cfa1965b07ef1040682c
nitin devatraj <nitindevatraj@protonmail.com>
2024-01-25 18:55:14 +0530

    dispatch actions through action creators in AccountOperations and CreateCustomer components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import CreateCustomer from "../src/features/customers/CreateCustomer";
 import Customer from "../src/features/customers/Customer";
 import AccountOperations from "../src/features/accounts/AccountOperations";
 import BalanceDisplay from "../src/features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((state) => state.customer.fullName);
   return (
     <div>
       <h1>Juno Capital</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+
+      {fullName === "" ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
